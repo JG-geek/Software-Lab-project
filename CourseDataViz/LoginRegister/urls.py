b408ee1f5b39f28b755f02deb56bbff72551202a
Aditya Hiteshbhai Kansara <adityakansara@cse.iitb.ac.in>
2023-11-01 12:20:40 +0530

    feat: added login-register functionality

--- CourseDataViz/LoginRegister/urls.py
+++ CourseDataViz/LoginRegister/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name="index"),
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout, name="logout"),
+    path('home/', views.home, name="home"),
+    path('secret/', views.secret, name="secret"),
 ]
--- CourseDataViz/LoginRegister/views.py
+++ CourseDataViz/LoginRegister/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User,Group
+from django.contrib.auth.models import User,Group,auth
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 # Create your views here.
 def index(request):
-    # user = User.objects.get()
-    g = Group.objects.get(name="Student")
-    user = User.objects.create_user(
-        "john", "lennon@thebeatles.com", "johnpassword")
-    user.groups.add(g)
-    user.save()
+    return render(request,'home.html')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+
+        if (user is not None):
+            auth.login(request,user)
+            return redirect('/home')       
+        else:
+            return HttpResponse("Invalid login credentials")
     
-    return HttpResponse(user.groups.get())
+def register(request):
+    if request.method == 'GET':
+        return render(request,'register.html')
+    else:
+        username = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        password = request.POST['password']
+        email = request.POST['email']
+        user = User.objects.create_user(username=username,first_name=first_name, last_name=last_name,password=password,email=email)
+        user.save()
+        return redirect('/login')
+
+def home(request):
+    return render(request,'home.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+@login_required(login_url='/getLoginPage/')
+def secret(request):
+    return render(request, 'secret.html')
